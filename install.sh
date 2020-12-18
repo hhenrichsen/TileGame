ad93bfeb663083482ce2c6c76d1099350aebc500
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-12-18 03:35:38 -0700

    :hammer: Add repos in install script.

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add skypjack https://api.bintray.com/conan/skypjack/conan
 conan install . -s build_type=Release --install-folder=cmake-build-debug
